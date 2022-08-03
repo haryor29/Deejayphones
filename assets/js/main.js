1dc4494c78e9a4c812ac711bfdaf08ef243bad6a
unknown <ayodejiomogunloye@gmail.com>
2022-08-03 10:50:34 +0100

    index page

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false
+    }
+  });
+
+  AOS.init();
